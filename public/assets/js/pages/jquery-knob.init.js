0dd0f8ea28d4087a4277c05840ac28f0e3b7305a
Yudo <53389941+juniz@users.noreply.github.com>
2023-10-14 22:54:24 +0700

    fix

--- public/assets/js/pages/jquery-knob.init.js
+++ public/assets/js/pages/jquery-knob.init.js
@@ -1,18 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!************************************************!*\
-  !*** ./resources/js/pages/jquery-knob.init.js ***!
-  \************************************************/
-/*
-Template Name: Dason - Admin & Dashboard Template
-Author: Themesdesign
-Website: https://themesdesign.in/
-Contact: themesdesign.in@gmail.com
-File: Jquery knob init Js File
-*/
-
-$(function () {
-  $(".knob").knob();
-});
-/******/ })()
-;
+$((function(){$(".knob").knob()}));
