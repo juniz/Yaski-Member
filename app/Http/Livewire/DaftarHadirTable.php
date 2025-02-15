5d35fd19cacd3c686cf4313730826b429f8910b9
Yudo <53389941+juniz@users.noreply.github.com>
2025-02-16 00:03:17 +0700

    tambah pengaturan binary Node dan NPM untuk Browsershot

--- app/Http/Livewire/DaftarHadirTable.php
+++ app/Http/Livewire/DaftarHadirTable.php
@@ -168,6 +168,8 @@ class DaftarHadirTable extends DataTableComponent
             'data' => $data,
         ])->render();
         Browsershot::html($template)
+            ->setNodeBinary('/root/.nvm/versions/node/v22.13.0/bin/node')
+            ->setNpmBinary('/root/.nvm/versions/node/v22.13.0/bin/npm')
             ->showBackground()
             ->format('A5')
             ->pages('1')
