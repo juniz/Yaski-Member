4b86bf2fac67a09e09a96f978cf32e40b6a2c0d6
Yudo <53389941+juniz@users.noreply.github.com>
2025-02-16 10:58:16 +0700

    perbarui include path untuk Browsershot di DaftarHadirTable

--- app/Http/Livewire/DaftarHadirTable.php
+++ app/Http/Livewire/DaftarHadirTable.php
@@ -168,7 +168,7 @@ class DaftarHadirTable extends DataTableComponent
             'data' => $data,
         ])->render();
         Browsershot::html($template)
-            ->setIncludePath('$PATH:/home/yaskimember.org/bin')
+            ->setIncludePath('$PATH:/home/yaskimember.org/node/v22.13.0/bin')
             ->showBackground()
             ->format('A5')
             ->pages('1')
